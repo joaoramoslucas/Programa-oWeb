d4340fdbb98b6421d37545ed4672d30270082408
joaoramoslucas <joaopedroramosdelucas10@gmail.com>
2024-08-13 11:54:05 -0300

    Novos Ajustes e novas funcionalidades

--- index.js
+++
@@ -1,9 +0,0 @@
-const button = document.querySelector("button")
-const p = document.querySelector("p")
-
-function clicou() {
-    alert("Hello, world")
-    p.textContent = "Obrigado por clicar"
-}
-
-button.onclick = clicou
